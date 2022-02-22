dc9753e95e3e780f63c46ed339af1dc06dfc4ab5
Vicente Garcia <vicen1@hotmail.com>
2022-02-22 17:12:35 -0600

    Add prevent default

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,15 @@
 /* Date     : 02/22/2022     */
 /* Modified : 02/22/2022     */
 /* ------------------------- */
-var buttonEl = document.querySelector("#save-task");
+//var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(){
+    event.preventDefault();
     var listItemEl = document.createElement("li");
     listItemEl.textContent = "This is a new task.";
     listItemEl.className = "task-item";
     tasksToDoEl.appendChild(listItemEl);
 }
-buttonEl.addEventListener("click", createTaskHandler);
+//buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
