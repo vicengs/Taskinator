450209320af0295fcdf04ddf899fd556b0775381
Vicente Garcia <vicen1@hotmail.com>
2022-02-24 12:01:57 -0600

    Add items to form

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,26 @@
 /* File     : script.js      */
 /* Author   : Vicente Garcia */
 /* Date     : 02/22/2022     */
-/* Modified : 02/22/2022     */
+/* Modified : 02/24/2022     */
 /* ------------------------- */
 //var buttonEl = document.querySelector("#save-task");
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(){
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // Create list item
     var listItemEl = document.createElement("li");
-    listItemEl.textContent = "This is a new task.";
     listItemEl.className = "task-item";
+    //listItemEl.textContent = taskNameInput;
+    // Create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    // Add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    // Add elements to parent
+    listItemEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(listItemEl);
 }
 //buttonEl.addEventListener("click", createTaskHandler);
